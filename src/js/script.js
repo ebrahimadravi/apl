1795a96dcdec4ac8f78656339d4a4275e88954fc
Mohd Ebrahim <mohd.ebrahim.jmi@gmail.com>
2024-02-07 22:31:37 +0530

    oil seed page done

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,18 @@
 $(document).ready(function () {
-  $('.site-header').click(function() {
-    $('.mobile-header').toggle();
-  })
+  $(".site-header").click(function () {
+    $(".mobile-header").toggle();
+  });
+
+  // sticky jeader
+
+  var header = $("header");
+  var headerOffset = header.offset().top;
+
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > headerOffset) {
+      header.addClass("sticky");
+    } else {
+      header.removeClass("sticky");
+    }
+  });
 });
