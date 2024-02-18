90e39348f277590a6fe31958311e3c45f3ebc35d
Mohd Ebrahim <mohd.ebrahim.jmi@gmail.com>
2024-02-18 14:29:48 +0530

    contat us pge done and some enhancement on home page

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,14 @@
 $(document).ready(function () {
   $(".site-header").click(function () {
-    $(".mobile-header").toggle();
+    $('body').addClass('open-mobile-menu');
   });
-
+  $('.fa-xmark').click(function() {
+    $('body').removeClass('open-mobile-menu');
+  })
   // sticky jeader
 
   var header = $("header");
   var headerOffset = header.offset().top;
-
   $(window).scroll(function () {
     if ($(window).scrollTop() > headerOffset) {
       header.addClass("sticky");
@@ -15,4 +16,48 @@ $(document).ready(function () {
       header.removeClass("sticky");
     }
   });
+
+  $(".selling-product").slick({
+    infinite: true,
+    slidesToShow: 7,
+    slidesToScroll: 1,
+    prevArrow:
+      '<div class="slide-arrow prev-arrow"><i class="fa-solid fa-chevron-left"></i></div>',
+    nextArrow:
+      '<div class="slide-arrow next-arrow"><i class="fa-solid fa-chevron-right"></i></div>',
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1,
+          adaptiveHeight: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          adaptiveHeight: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+  $('.testimonial-section ul li .slider').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+  })  
 });
